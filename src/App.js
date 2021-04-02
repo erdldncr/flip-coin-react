289eedbaf725db1609b82b1fef1683a96fea6816
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-04-02 05:41:46 +0100

    flip-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const imageSource=['https://wi-images.condecdn.net/image/kMw9dD3kMqD/crop/900/f/pound-coin.png','https://wi-images.condecdn.net/image/zGpMQNjL1bN/crop/900/f/pound-coin-front.png']
+  const [image,setImage]=useState(null)
+  const [countFlip, setCountFlip]=useState(0)
+  const [countHeads, setCountHeads]=useState(0)
+  const [countTails, setCountTails]=useState(0)
+  
+  function checkHead(){
+    setCountFlip(countFlip+1)
+    let number=Math.floor(Math.random()*2)
+    setImage(imageSource[number])
+    number===0?setCountHeads(countHeads+1):setCountTails(countTails+1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let'sFlip a coin</h1>
+        <div>
+        {image&&<img src={image} alt=""/>}
+        </div>
+      
+      <button onClick={checkHead}>Flip me</button>
+      <p>Out of {countFlip} flips, there have been {countHeads} heads and {countTails} tails</p>
     </div>
   );
 }
